e310c04c7cce74dcbbc3f263f2c5bc3eb78f0cd7
Noah Bathras <nbathras@terpmail.umd.edu>
2021-12-06 21:38:55 -0500

    Day1 pt2

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
 use std::{
     fs::File,
     io::{prelude::*, BufReader},
-    path::Path,
+    path::Path
 };
-
-fn get_stripped_user_input(user_input: &mut String) {
-    std::io::stdin().read_line(user_input).expect("Failed to read user input");
-    user_input.retain(|c| !c.is_whitespace());
-}
 
 fn get_lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
     let file = File::open(filename).expect("no such file");
@@ -17,22 +12,9 @@ fn get_lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
         .collect()
 }
 
-fn get_filename() -> String {
-    let mut user_input = String::new();
-    println!("Which input file should we use.  Type '1' for example input and '2' for test input: ");
-    get_stripped_user_input(&mut user_input);
-    return match user_input.as_str() {
-        "1" => String::from("src/inputExample.txt"),
-        _ => String::from("src/input.txt")
-    }
-}
-
 // ---
 
-fn main() {
-    let filename = get_filename();
-    let lines = get_lines_from_file(filename);
-
+fn part1(lines: &Vec<String>) -> i32 {
     let mut previous_depth = 0;
     let mut increase_counter = 0;
 
@@ -46,5 +28,46 @@ fn main() {
         previous_depth = new_depth;
     }
 
-    println!("{:?}", increase_counter - 1);
+    increase_counter - 1
+}
+
+fn part2(lines: &Vec<String>) -> i32 {
+    let mut previous_depth = 0;
+    let mut increase_counter = 0;
+    
+    for i in 0..lines.len() {
+        if i + 2 >= lines.len() { break; }
+
+        let new_depth1 = lines[i].parse::<i32>().expect("Could not convert line to i32");
+        let new_depth2 = lines[i+1].parse::<i32>().expect("Could not convert line to i32");
+        let new_depth3 = lines[i+2].parse::<i32>().expect("Could not convert line to i32");
+        let new_depth = new_depth1 + new_depth2 + new_depth3;
+
+        if new_depth > previous_depth {
+            increase_counter += 1;
+        }
+
+        previous_depth = new_depth;
+    }
+
+    increase_counter - 1
+}
+
+fn compute_solutions(lines: Vec<String>) {
+    println!("Part1: {:?}", part1(&lines));
+    println!("Part2: {:?}\n", part2(&lines));
+}
+
+fn compute_solution() {
+    println!("Example Input:");
+    let lines = get_lines_from_file("src/inputExample.txt");
+    compute_solutions(lines);
+
+    println!("Test Input:");
+    let lines = get_lines_from_file("src/input.txt");
+    compute_solutions(lines);
+}
+
+fn main() {
+    compute_solution();
 }
