f29be6f408798a7a81dfbcc886be648f586ead01
Noah Bathras <nbathras@terpmail.umd.edui>
2021-12-19 21:37:05 -0500

    Day5 pt1

--- src/day5/mod.rs
+++ src/day5/mod.rs
@@ -1,22 +1,14 @@
+use std::collections::HashMap;
+
 use crate::utils::get_lines_from_file;
 
 const INPUT_EXAMPLE_FILE_PATH: &str = "src/day5/inputExample.txt";
 const INPUT_TEST_FILE_PATH: &str = "src/day5/input.txt";
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
 struct Point {
     x: i32,
     y: i32,
-}
-
-impl Point {
-    fn get_x(&self) -> usize {
-        self.x.try_into().unwrap()
-    }
-
-    fn get_y(&self) -> usize {
-        self.y.try_into().unwrap()
-    }
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -78,29 +70,21 @@ fn build_line_segment(line: &String) -> LineSegment {
 }
 
 fn part1(lines: &Vec<String>) -> i32 {
-    let mut state = [[0; 1000]; 1000];
+    let mut map: HashMap<Point, i32> = HashMap::new();
 
     for line in lines {
         let line_segment = build_line_segment(line);
         for point in line_segment.get_line() {
-            state[point.get_x()][point.get_y()] += 1;
+            *map.entry(point).or_insert_with(|| 0) += 1;
         }
     }
     
     let mut danger_point_count = 0;
 
-    for y in 0..state.len() {
-        for x in 0..state[y].len() {
-            if state[x][y] == 0 {
-                print!(" . ");
-            } else {
-                print!(" {:?} ", state[x][y]);
-                if state[x][y] > 1 {
-                    danger_point_count += 1;
-                }
-            }
+    for (_, v ) in map.into_iter() {
+        if v > 1 {
+            danger_point_count += 1;
         }
-        println!("");
     }
 
     danger_point_count
