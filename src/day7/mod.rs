5d485b4438a99821324d2ff53707731c3b7fb114
Noah Bathras <nbathras@terpmail.umd.edui>
2021-12-26 14:27:16 -0500

    Day7 pt1 and improved input system

---
+++ src/day7/mod.rs
@@ -0,0 +1,78 @@
+use crate::utils::Input;
+
+const INPUT_EXAMPLE_FILE_PATH: &str = "src/day7/inputExample.txt";
+const INPUT_TEST_FILE_PATH: &str = "src/day7/input.txt";
+
+fn part1(file_name: &str) -> i32 {
+    let mut input = Input::init(file_name);
+    let positions: Vec<i32> = Input::split_and_parse(input.read_line(), ",");
+
+    let max_position: i32 = *positions.iter().max().expect("No max value found in position");
+
+    let mut min_fuel_cost = i32::MAX;
+
+    for target_position in 0..max_position+1 {
+        let mut fuel_cost = 0;
+
+        for j in 0..positions.len() {
+            fuel_cost += (positions[j] - target_position).abs();
+        }
+
+        if fuel_cost < min_fuel_cost {
+            min_fuel_cost = fuel_cost;
+        }
+    }
+
+    min_fuel_cost
+}
+
+fn part2(_file_name: &str) -> i32 {
+    -1
+}
+
+pub fn compute_solution() {
+    println!("Part 1:");
+    println!("Example Input:");
+    println!("{}", part1(INPUT_EXAMPLE_FILE_PATH));
+    println!("Test Input:");
+    println!("{}", part1(INPUT_TEST_FILE_PATH));
+
+    println!("Part 2:");
+    println!("Example Input:");
+    println!("{}", part2(INPUT_EXAMPLE_FILE_PATH));
+    println!("Test Input:");
+    println!("{}", part2(INPUT_TEST_FILE_PATH));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_example_input() {
+        let answer = part1(INPUT_EXAMPLE_FILE_PATH);
+
+        assert_eq!(answer, 37);
+    }
+
+    #[test]
+    fn part1_input() {
+        let answer = part1(INPUT_TEST_FILE_PATH);
+
+        assert_eq!(answer, 357353);
+    }
+
+    #[test]
+    fn part2_example_input() {
+        let answer = part2(INPUT_EXAMPLE_FILE_PATH);
+
+        assert_eq!(answer, answer);
+    }
+
+    #[test]
+    fn part2_input() {
+        let answer = part2(INPUT_TEST_FILE_PATH);
+
+        assert_eq!(answer, answer);
+    }
+}
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -11,3 +11,38 @@ pub fn get_lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
         .map(|l| l.expect("Could not parse line"))
         .collect()
 }
+
+pub struct Input {
+    reader: BufReader<File>,
+}
+
+impl Input {
+    pub fn init(file_name: &str) -> Input {
+        let file = File::open(file_name).expect("file could not be found/opened in current directory!");
+        Input {
+            reader: BufReader::new(file),
+        }
+    }
+
+    pub fn read_line(&mut self) -> String {
+        let mut tmp_line = String::new();
+        self.reader.read_line(&mut tmp_line).expect("Could not read line from file!");
+        tmp_line
+    }
+    
+    pub fn read_all(&mut self) -> String {
+        let mut tmp_text = String::new();
+        self.reader.read_to_string(&mut tmp_text).expect("Could not read all lines from file!");
+        tmp_text
+    }
+
+    pub fn split_and_parse<T: std::str::FromStr>(string: String, pattern: &str) -> Vec<T>
+        where <T as std::str::FromStr>::Err: std::fmt::Debug {
+        string.trim_end()
+              .split(pattern)
+              .map(|s| {
+                  s.trim_end().parse::<T>().expect("Could not parse element in file!")
+               })
+               .collect()
+    }
+}
